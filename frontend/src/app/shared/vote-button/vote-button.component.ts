734d7e8cf227e01fee20e91b1dd60a5c977492a7
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2020-08-16 16:54:47 +0530

    SI: Added voting functionality from frontend.

--- frontend/src/app/shared/vote-button/vote-button.component.ts
+++ frontend/src/app/shared/vote-button/vote-button.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PostModel } from '../post-model';
-import {faArrowUp, faArrowDown} from '@fortawesome/free-solid-svg-icons';
+import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { VotePayload } from './vote-payload';
+import { AuthService } from 'src/app/auth/shared/auth.service';
+import { PostService } from '../post.service';
+import { ToastrService } from 'ngx-toastr';
+import { VoteService } from '../vote.service';
+import { VoteType } from './vote-type';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-vote-button',
@@ -8,22 +15,51 @@ import {faArrowUp, faArrowDown} from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./vote-button.component.css']
 })
 export class VoteButtonComponent implements OnInit {
+  @Input() post: PostModel;
   faArrowUp = faArrowUp;
   faArrowDown = faArrowDown;
   upvoteColor: string;
   downvoteColor: string;
-  @Input() post: PostModel;
+  votePayload: VotePayload;
 
-  constructor() { }
+  constructor(private voteService: VoteService,
+    private authService: AuthService,
+    private postService: PostService,
+    private toastr: ToastrService) {
+    // Initialized votePayload with undefined values
+    this.votePayload = {
+      voteType: undefined,
+      postId: undefined
+    };
+  }
 
   ngOnInit(): void {
   }
 
   upvotePost() {
-    // TODO: Upvote Post
+    this.votePayload.voteType = VoteType.UPVOTE;
+    this.vote();
   }
 
   downvotePost() {
-    // TODO: Downvote Post
+    this.votePayload.voteType = VoteType.DOWNVOTE;
+    this.vote();
+  }
+
+  vote() {
+    this.votePayload.postId = this.post.id;
+
+    this.voteService.vote(this.votePayload).subscribe(() => {
+      this.updateVoteDetails();
+    }, error => {
+      this.toastr.error(error.error.message);
+      throwError(error);
+    });
+  }
+
+  private updateVoteDetails() {
+    this.postService.getPost(this.post.id).subscribe(post => {
+      this.post = post;
+    });
   }
 }
---
+++ frontend/src/app/shared/vote.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { VotePayload } from '../shared/vote-button/vote-payload';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VoteService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  vote(votePayload: VotePayload): Observable<any> {
+    return this.httpClient.post('http://localhost:8080/api/votes/', votePayload);
+  }
+}
